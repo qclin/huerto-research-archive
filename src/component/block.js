60c30fcb88fe84dff84f8ba4aee97b63a332aa2c
qclin <qiaoclin@gmail.com>
2021-06-07 10:39:27 +0200

    refactor to blocks

--- src/component/block.js
+++ src/component/block.js
@@ -1,6 +1,32 @@
 import * as React from "react"
 
 
-function Block(){
-  
+function Block({field, media}){
+  console.log("block media", media)
+  return(
+    <dd className="border-r flex-shrink-0 w-80 hover:bg-melone">
+      <p className="border-b uppercase align-middle bg-eggwash text-base px-4 hover:bg-melone">
+        {field.TITLE}
+        <span className="float-right text-sm">
+          {field.MEDIA_TYPE}
+        </span>
+      </p>
+      {media &&
+      <div  className="h-100">
+        <iframe src={media.URL}
+          width="100%"
+          height="100%"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          frameBorder="0"
+          webkitallowfullscreen="true"
+          mozallowfullscreen="true"
+          allowFullScreen
+          title={media.Title}
+        />
+      </div>
+      }
+    </dd>
+  )
 }
+
+export default Block;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-import {groupByCategory} from "../utils/constants";
-
+import { groupByCategory } from "../utils/constants";
+import Block from "../component/block";
 
 const IndexPage = ({data}) => {
 
@@ -11,32 +11,14 @@ const IndexPage = ({data}) => {
       <div className="uppercase w-32 sticky left-0 text-base font-bold px-4 border-r">ARCHIVE</div>
     {Object.keys(groupFields).map((type, idx) => (
       <dl key={`row.${idx}`} className="h-64 border-b flex flex-row w-screen overflow-x-scroll">
-          <dt key={type} className="border-r flex-shrink-0 w-32 sticky left-0 bg-eggwash" >
-            <div className="uppercase text-base px-4 border-b">
-              {type}
-            </div>
-          </dt>
-          {groupFields[type].map((field, index) => {
-            return(
-              <dd key={`${field.IDENTIFIER}.${index}`} className="border-r flex-shrink-0 w-80 hover:bg-melone">
-                <p className="border-b uppercase align-middle bg-eggwash text-base px-4 hover:bg-melone">{field.TITLE} <span className="float-right text-sm">{field.MEDIA_TYPE}</span></p>
-                {field.MEDIA && field.MEDIA[0].data &&
-                <div  className="h-100">
-                  <iframe src={field.MEDIA[0].data?.URL}
-                    width="100%"
-                    height="100%"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    frameBorder="0"
-                    webkitallowfullscreen="true"
-                    mozallowfullscreen="true"
-                    allowFullScreen
-                    title={field.MEDIA[0].data?.Title}
-                  />
-                </div>
-                }
-              </dd>
-            )}
-          )}
+        <dt key={type} className="border-r flex-shrink-0 w-32 sticky left-0 bg-eggwash" >
+          <div className="uppercase text-base px-4 border-b">
+            {type}
+          </div>
+        </dt>
+        {groupFields[type].map((field, index) => (
+          <Block key={`${field.IDENTIFIER}.${index}`} field={field} media={field.MEDIA && field.MEDIA[0].data} />)
+        )}
         </dl>
       ))}
 
